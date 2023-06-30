ab9bbcb83c706bc3ec9aa41f0e50677d94a1e651
shivajid <shivaji@aurius.io>
2023-06-30 02:18:31 +0000

    Fixed the Wikipedia Webapp and hosted it

--- tutorials/use_case/WikiPedia/webapp/WikipediaAskv2.py
+++ tutorials/use_case/WikiPedia/webapp/WikipediaAskv2.py
@@ -17,44 +17,63 @@ import pandas
 from google.cloud import bigtable
 from google.cloud import happybase
 import timeit
+from google.oauth2 import service_account
+from google.cloud import aiplatform_v1beta1
+
 from datetime import datetime
 
-
-st.image("https://ichef.bbci.co.uk/news/976/cpsprodpb/12DE1/production/_123218277_gettyimages-1368388509.jpg", width=130)
+icon = "Wikipedia-logo-v2@2x.png"
+st.set_page_config(page_title="Wikipedia search", page_icon=icon )
+col1, col2, col3 = st.columns(3)
+with col2:
+ st.image("https://ichef.bbci.co.uk/news/976/cpsprodpb/12DE1/production/_123218277_gettyimages-1368388509.jpg", width=250)
 #st.write("xxxThe Corpus is on 2022 Winter Olympics Wikipedia Scraped Articles. The Application is powered by PaLM TextGeneration, Embedding, Matching Enging and BigTable")
 #st.write("The response is slow, please have patience as generating text takes time...")
 st.write("Ask a question on 2022 Winter Olympics")
 
-PROJECT_ID = "demogct2022" #@param
-INSTANCE_ID = "bus-instance" #@param
-TABLE_ID = "masterdata" #@param
-COLUMN_FAMILY_NAME = "cf1" #@param
-client = bigtable.Client(project=PROJECT_ID, admin=True)
-instance = client.instance(INSTANCE_ID)
-connection = happybase.Connection(instance=instance)
-table = connection.table(TABLE_ID)
-column_family_name = "cf1"
-column_name = "{fam}:content".format(fam=column_family_name)
+
+@st.cache_resource(show_spinner=False)
+def get_project_details():
+ PROJECT_ID = "demogct2022" #@param
+ INSTANCE_ID = "bus-instance" #@param
+ TABLE_ID = "masterdata" #@param
+ COLUMN_FAMILY_NAME = "cf1" #@param
+ return PROJECT_ID, INSTANCE_ID, TABLE_ID, COLUMN_FAMILY_NAME
+
+@st.cache_resource(show_spinner=False)
+def get_BigTable_table():
+ PROJECT_ID, INSTANCE_ID, TABLE_ID, COLUMN_FAMILY_NAME = get_project_details()
+ client = bigtable.Client(project=PROJECT_ID, admin=True)
+ instance = client.instance(INSTANCE_ID)
+ connection = happybase.Connection(instance=instance)
+ table = connection.table(TABLE_ID)
+ column_family_name = "cf1"
+ column_name = "{fam}:content".format(fam=column_family_name)
+ return table, column_name
 
 #Bigtable prompt logger
-plog_INSTANCE_ID = "inferresults"
-plog_TABLE_ID = "promptlogger"
-plog_COLUMN_FAMILY_NAME = "cf1" 
-appname = "WikipediaAskv2.py"
+@st.cache_resource(show_spinner=False)
+def get_logging_table():
+ plog_INSTANCE_ID = "inferresults"
+ plog_TABLE_ID = "promptlogger"
+ plog_COLUMN_FAMILY_NAME = "cf1" 
+ #appname = "WikipediaAskv2.py"
+ PROJECT_ID = "demogct2022" 
+ #Connect to Prompt Logger Table
+ pclient = bigtable.Client(project=PROJECT_ID, admin=True)
+ pinstance = pclient.instance(plog_INSTANCE_ID)
+ pconnection = happybase.Connection(instance=pinstance)
+ ptable = pconnection.table(plog_TABLE_ID)
+ pcolumn_family_name = plog_COLUMN_FAMILY_NAME
+ pcolumn_name = "{fam}:prompt".format(fam=pcolumn_family_name)
+ rcolumn_name = "{fam}:resp".format(fam=pcolumn_family_name)
+ return ptable, pcolumn_name, rcolumn_name
 
-#Connect to Prompt Logger Table
-pclient = bigtable.Client(project=PROJECT_ID, admin=True)
-pinstance = pclient.instance(plog_INSTANCE_ID)
-pconnection = happybase.Connection(instance=pinstance)
-ptable = pconnection.table(plog_TABLE_ID)
-pcolumn_family_name = plog_COLUMN_FAMILY_NAME
-pcolumn_name = "{fam}:prompt".format(fam=pcolumn_family_name)
-rcolumn_name = "{fam}:resp".format(fam=pcolumn_family_name)
+@st.cache_resource(show_spinner=False)
+def get_text_embedding_model():
+ model = TextEmbeddingModel.from_pretrained("textembedding-gecko@001")
+ return model
 
-model = TextEmbeddingModel.from_pretrained("textembedding-gecko-001")
-
-from google.oauth2 import service_account
-from google.cloud import aiplatform_v1beta1
 
 def _findneighbor_sample( val,
                          api_endpoint="563741590.us-central1-353713988661.vdb.vertexai.goog",
@@ -102,6 +121,7 @@ import json
 def query_embedding(
     query: str
 ):
+ model = get_text_embedding_model()
  return model.get_embeddings([query])
 
 
@@ -124,13 +144,13 @@ def get_date_time():
 def get_prow_key():
    #rkey = "wikipedia_string" + "#" + str(index).zfill(max_size)
    dt = get_date_time()
+   appname = "WikipediaAskv2.py"
    key_prefix = f"prompt_logger#{appname}"
    rkey = key_prefix + "#" + dt
    return rkey
 
 def putTable(table, pcolumn_name, prompt, rcolumn_name, resp ):
   row_key = get_prow_key()
-
   table.put(row_key, {pcolumn_name.encode("utf-8"): prompt.encode("utf-8"), rcolumn_name.encode("utf-8"): resp.encode("utf-8")})
   print ("Data Entered")
   return row_key
@@ -140,11 +160,12 @@ def putTable(table, pcolumn_name, prompt, rcolumn_name, resp ):
 
 
 # Invoke the LLM model
-#from google.cloud import aiplatform
-#from google.cloud.aiplatform.gapic.schema import predict
-#from google.protobuf import json_format
-#from google.protobuf.struct_pb2 import Value
-#from langchain.prompts import PromptTemplate
+
+@st.cache_resource(show_spinner=False)
+def get_textgen_model(PROJECT_ID, location, model_name):
+  vertexai.init(project=PROJECT_ID, location=location)
+  model = TextGenerationModel.from_pretrained(model_name)
+  return model
 
 def predict_large_language_model_sample(
     project_id: str,
@@ -158,8 +179,7 @@ def predict_large_language_model_sample(
     tuned_model_name: str = "",
     ) :
     """Predict using a Large Language Model."""
-    vertexai.init(project=project_id, location=location)
-    model = TextGenerationModel.from_pretrained(model_name)
+    model = get_textgen_model(project_id, location, model_name)
     if tuned_model_name:
       model = model.get_tuned_model(tuned_model_name)
     response = model.predict(
@@ -176,12 +196,13 @@ def predict_large_language_model_sample(
 
 max_size = 4
 def lookup_text_by_id(id):
+ table, column_name =get_BigTable_table()
  rkey = get_row_key(id, max_size)
  b_rkey=rkey.encode("utf-8")
  row = table.row(b_rkey)
  return row[b'cf1:content'].decode("utf-8")
 
-Question = "List the winners of the 2022 Winter Olympics: "
+
 
 def get_match_id_embedding(val):
  ids = []
@@ -202,6 +223,7 @@ def get_prompt_text_from_matches(match_ids) :
 
 def ask( Question,tok):
   #start_time = timeit.default_timer()
+
   val = query_embedding(Question)
   #elapsed = timeit.default_timer() - start_time
   #print ("Elapsed Time query_embedding:", elapsed)
@@ -218,7 +240,7 @@ def ask( Question,tok):
 
   prompt = "Based on the text below, " + Question+ "\n context:" + prompt_text
   predictions_text =predict_large_language_model_sample(
-      "demogct2022", "text-bison@001", 0.2, tok, 0.8, 40,
+      "demogct2022", "text-bison@latest", 0.2, tok, 0.8, 40,
           prompt,
        "us-central1")
   
@@ -227,6 +249,8 @@ def ask( Question,tok):
   return predictions_text
 
 #ask("When city wass Irina Avvakumova born?")
+#Question = "List the winners of the 2022 Winter Olympics: "
+Question = ""
 st.text_area("Enter your Question:", key="name", height=200, value=Question)
 with st.sidebar:
     st.slider("Max Tokens", min_value=128, max_value=1024,key="tok", step=16, value=512)
@@ -234,20 +258,17 @@ with st.sidebar:
 content =  st.session_state.name
 tok = st.session_state.tok
 
-if content is None or content == "":
-    content = Question
-
-with st.spinner("PROCESSING. Please wait....."):
-    start_time = timeit.default_timer()
-    response = ask(content, tok)
-    elapsed2 = timeit.default_timer() - start_time
-    st.write("Elapsed Time:", elapsed2)
-
-
-st.write(response)
-
-if response is not None or response.text != "": 
- putTable(ptable, pcolumn_name, content, rcolumn_name, response.text)
+if content != "":
+    #content = Question
+    with st.spinner("PROCESSING. Please wait....."):
+     start_time = timeit.default_timer()
+     response = ask(content, tok)
+     elapsed2 = timeit.default_timer() - start_time
+     st.write("Elapsed Time:", elapsed2)
+     st.write(response)
+     if response is not None or response.text != "": 
+      ptable, pcolumn_name, rcolumn_name = get_logging_table()
+      putTable(ptable, pcolumn_name, content, rcolumn_name, response.text)
 
 
 
--- tutorials/use_case/WikiPedia/webapp/pages/Prompt_History.py
+++ tutorials/use_case/WikiPedia/webapp/pages/Prompt_History.py
@@ -5,21 +5,25 @@ from google.cloud import happybase
 import pandas as pd
 import streamlit as st
 
-PROJECT_ID="demogct2022"#@param
+
 
 #Bigtable
-plog_INSTANCE_ID = "inferresults" #@param
-plog_TABLE_ID = "promptlogger" #@param
-plog_COLUMN_FAMILY_NAME = "cf1" #@param
-appname = "TextLLM"#@param
+@st.cache_resource(show_spinner=False)
+def _get_Big_Table():
+  PROJECT_ID="demogct2022"#@param
+  plog_INSTANCE_ID = "inferresults" #@param
+  plog_TABLE_ID = "promptlogger" #@param
+  plog_COLUMN_FAMILY_NAME = "cf1" #@param
+  appname = "TextLLM"#@param
 
-client = bigtable.Client(project=PROJECT_ID, admin=True)
-instance = client.instance(plog_INSTANCE_ID)
-connection = happybase.Connection(instance=instance)
-table = connection.table(plog_TABLE_ID)
-column_family_name = plog_COLUMN_FAMILY_NAME
-pcolumn_name = "{fam}:prompt".format(fam=column_family_name)
-rcolumn_name = "{fam}:resp".format(fam=column_family_name)
+  client = bigtable.Client(project=PROJECT_ID, admin=True)
+  instance = client.instance(plog_INSTANCE_ID)
+  connection = happybase.Connection(instance=instance)
+  table = connection.table(plog_TABLE_ID)
+  column_family_name = plog_COLUMN_FAMILY_NAME
+  pcolumn_name = "{fam}:prompt".format(fam=column_family_name)
+  rcolumn_name = "{fam}:resp".format(fam=column_family_name)
+  return table,pcolumn_name, rcolumn_name
 
 def get_date_time():
    return  datetime.today().strftime('%Y-%m-%d#%H:%M:%S')
@@ -38,14 +42,16 @@ def putTable(table, pcolumn_name, prompt, rcolumn_name, resp ):
   print ("Data Entered")
   return row_key
 
-patt = "prompt_logger#WikipediaAskv2".encode("utf-8")
-row = table.scan(row_prefix=patt)
+def _print_rows():
+ patt = "prompt_logger#WikipediaAskv2".encode("utf-8")
+ table, pcolumn_name, rcolumn_name = _get_Big_Table()
+ row = table.scan(row_prefix=patt)
 
-df = pd.DataFrame()
-parray = []
-resparray = []
+ df = pd.DataFrame()
+ parray = []
+ resparray = []
 
-for key, data in row:
+ for key, data in row:
   text = data[b'cf1:prompt'].decode("utf-8")
   resp = data[b'cf1:resp'].decode("utf-8")
   #print(text)
@@ -54,8 +60,9 @@ for key, data in row:
   parray.append(text)
   resparray.append(resp)
 
-df["prompt"] = parray
-df["response"] = resparray
+ df["prompt"] = parray
+ df["response"] = resparray
+ Data_reverse_row_1 = df.iloc[::-1]
+ st.table(Data_reverse_row_1)
 
-st.table(df)
-
+_print_rows()
